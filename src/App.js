2bf96d61e7a9653e2f04614fd5f820d543c8cfe1
Aram Kim <nayulpapa0201@gmail.com>
2021-10-05 13:53:44 -0600

    created register page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from "./pages/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
@@ -10,7 +11,7 @@ function App() {
     <>
       <TopBar />
 
-      <Login />
+      <Register />
     </>
   );
 }
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,31 @@
+import "./register.css";
+
+export default function Register() {
+  return (
+    <div className="register">
+      <span className="register-title">Register</span>
+      <form className="register-form">
+        <label>Name</label>
+        <input
+          type="text"
+          className="register-input"
+          placeholder="Enter your name..."
+        />
+        <label>Email</label>
+        <input
+          type="text"
+          className="register-input"
+          placeholder="Enter your email..."
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="register-input"
+          placeholder="Enter your password..."
+        />
+        <button className="register-btn">Register</button>
+      </form>
+      <button className="register-login-btn">Login</button>
+    </div>
+  );
+}
